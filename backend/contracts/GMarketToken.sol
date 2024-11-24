a7912eab85f5a5feaeeaba37ff0d719844348d13
Oleg Likhogub <oligli@yandex.ru>
2024-11-24 20:08:20 +0300

    Added GMarketToken.sol

---
+++ backend/contracts/GMarketToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GMarketToken is ERC20 {
+    constructor() ERC20("GMarketToken", "GMT") {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+}
