d5ad39777dd0a8a7f1e56587c70bd61c5f43b735
Oleg Likhogub <oligli@yandex.ru>
2024-11-25 14:44:01 +0300

    Refactored methods

--- backend/contracts/GMarketAuction.sol
+++ backend/contracts/GMarketAuction.sol
@@ -10,95 +10,109 @@ contract GMarketAuction {
         uint price;
     }
 
+    address public _owner;
     uint public _lastLotId;
     uint[] public _lotIds;
     address[] public _sellers;
     mapping(uint => Lot) public _lots;
     mapping(address => uint[]) public _sellerLotIds;
 
+    constructor() {
+        _owner = msg.sender;
+    }
+
     function createLot(string memory itemId, uint price) external returns (uint) {
+        return createLotInternal(msg.sender, itemId, price);
+    }
+
+    function createLot(address seller, string memory itemId, uint price) external returns (uint) {
+        require(_owner == msg.sender, "You are not the owner of the contract");
+        return createLotInternal(seller, itemId, price);
+    }
+
+    function cancelLot(uint lotId) external {
+        cancelLotInternal(msg.sender, lotId);
+    }
+
+    function cancelLot(address seller, uint lotId) external {
+        require(_owner == msg.sender, "You are not the owner of the contract");
+        cancelLotInternal(seller, lotId);
+    }
+
+    function getMyLots() external view returns (Lot[] memory) {
+        return getLotsBySellerInternal(msg.sender);
+    }
+
+    function getLotsBySeller(address seller) external view returns (Lot[] memory) {
+        require(_owner == msg.sender, "You are not the owner of the contract");
+        return getLotsBySellerInternal(seller);
+    }
+
+    function getLots() external view returns (Lot[] memory) {
+        return getLotsByIds(_lotIds);
+    }
+
+    function getSellers() external view returns (address[] memory) {
+        return _sellers;
+    }
+
+    function createLotInternal(address seller, string memory itemId, uint price) internal returns (uint) {
         uint lotId = ++_lastLotId;
         _lotIds.push(lotId);
-        _lots[lotId] = Lot({id: lotId, seller: msg.sender, itemId: itemId, price: price});
-        _sellerLotIds[msg.sender].push(lotId);
-        if (_sellerLotIds[msg.sender].length == 1) {
-            _sellers.push(msg.sender);
+        _lots[lotId] = Lot({id: lotId, seller: seller, itemId: itemId, price: price});
+        _sellerLotIds[seller].push(lotId);
+        if (_sellerLotIds[seller].length == 1) {
+            _sellers.push(seller);
         }
         return lotId;
     }
 
-    function cancelLot(uint lotId) external returns (bool) {
+    function cancelLotInternal(address seller, uint lotId) internal {
         Lot memory lot = _lots[lotId];
-        if (lot.id == 0) {
-            return false;
-        }
-        if (lot.seller != msg.sender) {
-            return false;
-        }
+        require(lot.id != 0, "Lot not found");
+        require(lot.seller == seller, "Wrong seller specified");
         delete _lots[lotId];
         deleteElement(_lotIds, lotId);
         deleteElement(_sellerLotIds[msg.sender], lotId);
         if (_sellerLotIds[msg.sender].length == 0) {
             deleteElement(_sellers, msg.sender);
         }
-        return true;
     }
 
-    function getAllLots() external view returns (Lot[] memory) {
-        Lot[] memory lots = new Lot[](_lotIds.length);
-        for (uint i = 0; i < _lotIds.length; i++) {
-            lots[i] = _lots[_lotIds[i]];
-        }
-        return lots;
+    function getLotsBySellerInternal(address seller) internal view returns (Lot[] memory) {
+        return getLotsByIds(_sellerLotIds[seller]);
     }
 
-    function getMyLots() external view returns (Lot[] memory) {
-        uint[] memory lotIds = _sellerLotIds[msg.sender];
-        Lot[] memory lots = new Lot[](lotIds.length);
+    function getLotsByIds(uint[] memory lotIds) internal view returns (Lot[] memory lots) {
+        lots = new Lot[](lotIds.length);
         for (uint i = 0; i < lotIds.length; i++) {
             lots[i] = _lots[lotIds[i]];
         }
-        return lots;
     }
 
-    function getAllSellers() external view returns (address[] memory) {
-        uint[] memory lotIds = _sellerLotIds[msg.sender];
-        address[] memory sellers = new address[](lotIds.length);
-        for (uint i = 0; i < lotIds.length; i++) {
-            sellers[i] = _lots[lotIds[i]].seller;
-        }
-        return sellers;
-    }
-
-    function deleteElement(uint[] storage array, uint value) internal returns (bool) {
+    function deleteElement(uint[] storage array, uint value) internal {
         if (array.length == 1 && array[0] == value) {
             array.pop();
-            return true;
         } else if (array.length > 1) {
             for (uint i = 0; i < array.length - 1; i++) {
                 if (value == array[i]) {
                     array[i] = array[array.length - 1];
                     array.pop();
-                    return true;
                 }
             }
         }
-        return false;
     }
 
-    function deleteElement(address[] storage array, address value) internal returns (bool) {
+    function deleteElement(address[] storage array, address value) internal {
         if (array.length == 1 && array[0] == value) {
             array.pop();
-            return true;
         } else if (array.length > 1) {
             for (uint i = 0; i < array.length - 1; i++) {
                 if (value == array[i]) {
                     array[i] = array[array.length - 1];
                     array.pop();
-                    return true;
                 }
             }
         }
-        return false;
     }
 }
