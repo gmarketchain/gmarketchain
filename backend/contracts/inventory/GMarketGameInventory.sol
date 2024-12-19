129337f718d19916a0dac4068318752d5a795040
likhogub <oligli@yandex.ru>
2024-12-19 21:36:35 +0300

    upd

--- backend/contracts/inventory/GMarketGameInventory.sol
+++ backend/contracts/inventory/GMarketGameInventory.sol
@@ -5,7 +5,7 @@ import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract GMarketGameInventory is ERC1155 {
 
-    constructor() ERC1155("https://game.example/api/item/{id}.json") {
+    constructor() ERC1155("https://gmarket.com/api/item/{id}.json") {
         _mint(msg.sender, 1, 10000000, "");
         _mint(msg.sender, 2, 10000000, "");
         _mint(msg.sender, 3, 10000000, "");
